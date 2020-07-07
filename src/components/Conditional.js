c17f454ac65c2454beb76a34b28bd52afc594721
Barri <info@bdesignstudio.co.uk>
2020-07-07 16:36:58 +0100

    conditional inside return

--- src/components/Conditional.js
+++ src/components/Conditional.js
@@ -3,10 +3,16 @@ import React from 'react'
 const Conditional =( )=> {
 
   const value = false;
+
+  const returnValue =()=>{
+    return false;
+  }
     return (
       <div>
         
-          { value ?<div>Hello it is true</div>: null }
+          {/* { value ?<div>Hello it is true</div>: null } */}
+
+          { returnValue() ?<div>Hello it's a  true</div>: <div>Oops it's false</div> }
         
       </div>
     )
