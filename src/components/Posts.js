a4adb9fc69f39aa886f5dcb5c3fdb5fc9b5812bf
Barri <info@bdesignstudio.co.uk>
2020-07-07 17:21:58 +0100

    Using array and keys

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,29 +5,36 @@ import { Link } from 'react-router-dom'
 
 const Posts = ()=> {
 
-  const ids= [
-    {id:'1', name: 'Post 1'},
-    {id:'2', name: 'Post 2'},
-    {id:'3', name: 'Post 3'},
-  ]
+  // const ids= [
+  //   {id:'1', name: 'Post 1'},
+  //   {id:'2', name: 'Post 2'},
+  //   {id:'3', name: 'Post 3'},
+  // ]
 
-  const list = ids.map(item => {
-    return(
-      <span key={item.id}>
-        <link to={item.id}/>{item.name} <link/><br/>
-      </span>
-    )
+  // const list = ids.map(item => {
+  //   return(
+  //     <span key={item.id}>
+  //       <link to={item.id}/>{item.name} <link/><br/>
+  //     </span>
+  //   )
 
-  })
-  return (
-    <div>
-      {list }
+  // })
+//   return ids.map(item => {
+//     return(
+//       <span key={item.id}>
+//         <link to={item.id}/>{item.name} <link/><br/>
+//       </span>
+//     )
+  
+// })
 
-      {/* <Link to="/posts/1">Post 1</Link> <br/>
-      <Link to="/posts/2">Post 2</Link><br/>
-      <Link to="/posts/3">Post 3</Link><br/> */}
-            
-      </div>
-  )
+return [
+  // "Hello ", 
+  // "I am ",
+  // "a react app- array"
+  <div key="1">Hello</div> ,
+  <div key="2">I am </div>,
+  <div key="3">a react app - array</div>
+]
 }
 export default Posts;
