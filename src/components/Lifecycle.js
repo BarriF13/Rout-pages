474aa9634b0cec1fe914eca421ec5e778911e2fb
Barri <info@bdesignstudio.co.uk>
2020-07-07 16:23:23 +0100

    use all type of lifecycle

--- src/components/Lifecycle.js
+++ src/components/Lifecycle.js
@@ -19,13 +19,49 @@ export class LifeCycle extends Component {
     // document.querySelector('h3').style.color = 'purple';
   }
 
+  //-6 this one will run each time we re render
+  componentWillUpdate(){
+    
+    console.log('Before update')
+  }
 
-  //4- render  jxs
+  //-7 comes after re render
+  componentDidUpdate(){
+    console.log('After update');
+  }
+  
+  //8- for changes of the props and state-- return true or false
+  // shouldComponentUpdate(nextProps, nextState){
+  //   console.log(this.state.title);
+  //   console.log(nextState.title);
+  //   if(nextState.title === 'Title in the render step 4e'){
+  //     return false;
+  //   }
+  //   return false;
+  // }
+   
+  //9- component
+  componentWillReceiveProps(){
+    console.log('BEFORE RECEIVE PROPS ');
+  }
+
+  //10 Un mount
+  componentWillUnmount(){
+      console.log('UNMOUNT');
+
+  }
+
+
+  //4- render  jxs-- each time we setstate SPA will re-render the whole render
 
   render() {
+    console.log('RENDER');
     return (
       <div>
         <h3>{this.state.title}</h3>
+        <div onClick={
+          () => this.setState({title: 'Title in the render step 4'})
+        }> Click to change title</div>
       </div>
     )
   }
