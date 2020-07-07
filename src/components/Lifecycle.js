158a20386f482bf1c9ff1190a64a1ef868399fe3
Barri <info@bdesignstudio.co.uk>
2020-07-07 17:01:32 +0100

    Ready for pure component

--- src/components/Lifecycle.js
+++ src/components/Lifecycle.js
@@ -8,48 +8,48 @@ export class LifeCycle extends Component {
   state = {
     title: 'life cycles'
   }
-    //5 - after render
-    componentDidMount(){
-      console.log('5 after render');
-      document.querySelector('h3').style.color = 'purple';
-    }
-  //3- before render--this is always called before the render
-  componentWillMount(){
-    console.log('3- before the render');
-    // document.querySelector('h3').style.color = 'purple';
-  }
-
-  //-6 this one will run each time we re render
-  componentWillUpdate(){
-    
-    console.log('Before update')
-  }
-
-  //-7 comes after re render
-  componentDidUpdate(){
-    console.log('After update');
-  }
-  
-  //8- for changes of the props and state-- return true or false
-  // shouldComponentUpdate(nextProps, nextState){
-  //   console.log(this.state.title);
-  //   console.log(nextState.title);
-  //   if(nextState.title === 'Title in the render step 4e'){
-  //     return false;
+  //   //5 - after render
+  //   componentDidMount(){
+  //     console.log('5 after render');
+  //     document.querySelector('h3').style.color = 'purple';
   //   }
-  //   return false;
+  // //3- before render--this is always called before the render
+  // componentWillMount(){
+  //   console.log('3- before the render');
+  //   // document.querySelector('h3').style.color = 'purple';
   // }
+
+  // //-6 this one will run each time we re render
+  // componentWillUpdate(){
+    
+  //   console.log('Before update')
+  // }
+
+  // //-7 comes after re render
+  // componentDidUpdate(){
+  //   console.log('After update');
+  // }
+  
+  // //8- for changes of the props and state-- return true or false
+  // // shouldComponentUpdate(nextProps, nextState){
+  // //   console.log(this.state.title);
+  // //   console.log(nextState.title);
+  // //   if(nextState.title === 'Title in the render step 4e'){
+  // //     return false;
+  // //   }
+  // //   return false;
+  // // }
    
-  //9- component
-  componentWillReceiveProps(){
-    console.log('BEFORE RECEIVE PROPS ');
-  }
+  // //9- component
+  // componentWillReceiveProps(){
+  //   console.log('BEFORE RECEIVE PROPS ');
+  // }
 
-  //10 Un mount
-  componentWillUnmount(){
-      console.log('UNMOUNT');
+  // //10 Un mount
+  // componentWillUnmount(){
+  //     console.log('UNMOUNT');
 
-  }
+  // }
 
 
   //4- render  jxs-- each time we setstate SPA will re-render the whole render
@@ -60,8 +60,8 @@ export class LifeCycle extends Component {
       <div>
         <h3>{this.state.title}</h3>
         <div onClick={
-          () => this.setState({title: 'Title in the render step 4'})
-        }> Click to change title</div>
+          () => this.setState({title: 'something else'})
+        }> CLICK TO CHANGE</div>
       </div>
     )
   }
