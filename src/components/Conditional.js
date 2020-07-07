f9d3596ca61e3ea1b9f963b52a8d673a1b871f47
Barri <info@bdesignstudio.co.uk>
2020-07-07 16:33:55 +0100

    conditional rout

--- src/components/Conditional.js
+++ src/components/Conditional.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const Conditional =( )=> {
+
+  const value = false;
     return (
       <div>
+        
+          { value ?<div>Hello it is true</div>: null }
         
       </div>
     )
